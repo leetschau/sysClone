b123db3cc7409fffc8b52da1992ee67b33562ec6
Li Chao <leetschau@gmail.com>
2016-09-04 18:44:03 +0800

    make showiz usable

--- scripts/showiz.sh
+++ scripts/showiz.sh
@@ -1,26 +1,39 @@
 #!/bin/bash
 
-case $1 in
-  left) xpos=11
-        ypos=22;;
-  down) xpos=33
-        ypos=44;;
-  solo) xpos=55
-        ypos=66;;
-esac
-echo $xpos
-echo $ypos
+set_style() {
+  echo 'set style to <'$1'>'
+  gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'Show WizNote'
+  gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "showiz $1"
+  gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Primary>0'
+  sudo rm -f /usr/local/bin/showiz
+  sudo ln -s $0 /usr/local/bin/showiz
+}
 
+case $1 in
+  leftright) xpos=2954;;
+  updown) xpos=1590;;
+  solo) xpos=1066;;
+  set) set_style $2
+       exit 0;;
+esac
 
 if [ $# -eq 0 ]; then
-    echo  'Command format: showWiz <xpos>
-          Put the mouse on the WizNote icon and run `xdotool getmouselocation --shell`
-          <xpos> is the X value in the output.'
+    echo '
+Usage:
+1. Setup: run command `~/apps/scripts/showiz set updown|leftright|solo`
+   Invoke this command with ABSOLUTE path to make soft link right.
+2. Run: use keyboard shortcut Ctrl-0 to show WizNote panel.
+
+Note: When the size of the laptop or monitor changed,
+put the mouse on the WizNote icon, run `xdotool getmouselocation --shell`,
+and replace the X, Y coordinate here.'
     exit 1
 fi
 
-xpos=$1
-xdotool mousemove $1 9 click 3
+iconY=9
+menuY=31
+
+xdotool mousemove $xpos $iconY click 3
 sleep 0.2
-xdotool mousemove $1 31 click 1
+xdotool mousemove $xpos $menuY click 1
 xdotool search --class WizNote windowactivate
